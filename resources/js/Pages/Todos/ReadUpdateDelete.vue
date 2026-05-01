cadd7c3c35e84437fe2974f0e2370497ca822708
mariannehartigan <mariannehartigan@yahoo.ca>
2026-05-01 07:33:32 -0400

    added hidden to delete to make ux faster

--- resources/js/Pages/Todos/ReadUpdateDelete.vue
+++ resources/js/Pages/Todos/ReadUpdateDelete.vue
@@ -1,19 +1,33 @@
 <template>
   <form @change="update">
-    <input v-model="form.description" class="w-[28vw]" />
-    <input v-model="form.completed" type="checkbox" :true-value="1" :false-value="0" />
-    <Link :href="`/todo/${props.todo.id}`" method="DELETE" as="button" class="pl-[1vw]">
-      <img style="width: 0.8vw" src="../../../../public/trashcan.png" />
-    </Link>
+    <div v-if="!hidden">
+      <input v-model="form.description" class="w-[28vw]" />
+      <input v-model="form.completed" type="checkbox" :true-value="1" :false-value="0" />
+      <Link 
+        :href="`/todo/${props.todo.id}`" 
+        method="DELETE" 
+        as="button" 
+        class="pl-[1vw]"
+        @click="hide"
+      >
+        <img style="width: 0.8vw" src="../../../../public/trashcan.png" />
+      </Link>
+    </div>
   </form>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { Link, useForm } from '@inertiajs/vue3'
 
 const props = defineProps({
   todo: Object
 })
+
+const hidden = ref(false)
+const hide = () => {
+  hidden.value = true
+}
 
 const form = useForm({
   description: props.todo.description,
